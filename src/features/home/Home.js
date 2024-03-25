f3434defcf0bafda4bfb3733c0938cdbe681b33f
Mahendra-Thapa <thapamadenhdra671@Email.com>
2024-03-25 21:19:50 +0545

    add the hooks branch

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,66 +1,33 @@
-import { NavLink, useNavigate } from "react-router-dom"
-import { data } from "../../dummy/product"
-
-
+import { useState } from "react";
 
 
 const Home = () => {
 
-const nav = useNavigate();
-const person = {
-  name: 'ram'
-}
+const {text, setText} = useState('first value')
+// console.log(mi);
+// const [a, b] = mi;
+// console.log(a)
+// console.log(b)
 
 
+  let myMsg = 'some';
+  const handleChange = (msg) => {
+    setText (msg);
+    // console.log('call');
+    // myMsg = 'change on';
+    return msg;
+  }
+
+  console.log('render');
 
   return (
-    <div>
-  
-    {/* <div style={{ backgroundImage: "url('https://images.unsplash.com/photo-1707343848723-bd87dea7b118?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }} className="bg-no-repeat bg-cover bg-center bg-blend-darken h-[200px] w-full">
-    </div> */}
+    <div className="p-2">
+
+      <button onClick={() => handleChange('lio')} className="px-2  border-4 border-black  rounded-lg bg-black text-white font-serif font-boldrounded-lg">Click me</button>
+
+      <h1>hello text {text}</h1>
 
 
-
- 
-<div className="p-8 grid grid-cols-2  gap-6 ">
-        {data.products.map((product) => {
-          return <div
-            // onClick={() => nav(`/product/detail/${JSON.stringify(person)}`,)}
-
-              onClick={() => nav(`/product/detail/ ${product.id}`,)}
-
-
-            className="shadow-2xl p-3 cursor-pointer flex border-8 border-gray-700 bg-gray-300  hover:bg-gray-400 hover:white" key={product.id}>
-
-           <div className="p-5 ">
-
-              <img className="w-56 h-40 bg-cover " src={product.thumbnail}  />
-  
-              </div>
-  
-              <div className="w-96  p-2.5 ">
-             
-              <h3 className="text-2xl font-serif ">{`Price: Rs.${product.price}/-`}</h3>
-              <h3 className=" font-serif  text-2xl text-blue-700 font-semibold">{`Discount: ${product.discountPercentage}%`}</h3>
-            
-             
-             
-            </div>
-          </div>
-        })}
-      </div> 
-   
-
-
-
-
-
-
-
-        
-
-      {/* <h1>Home</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, unde labore iusto laudantium recusandae similique magnam sunt deleniti? Ullam, nostrum.</p> */}
     </div>
   )
 }
