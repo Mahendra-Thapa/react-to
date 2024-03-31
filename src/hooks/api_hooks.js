89a9380d6f7bb88e0c5b18f813445a3354bb963a
Mahendra-Thapa <thapamadenhdra671@Email.com>
2024-03-31 08:18:20 +0545

    changes

--- src/hooks/api_hooks.js
+++ src/hooks/api_hooks.js
@@ -10,7 +10,7 @@ export const useApiHooks = (params, id) => {
   const getData = async () => {
     try {
       setLoad(true);
-      const response = await axios.get("http://www.omdbapi.com", {
+      const response = await axios.get("https://www.omdbapi.com", {
         params: {
           apikey: "fc1d2de0",
           ...params
